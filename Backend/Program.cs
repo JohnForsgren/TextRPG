beed9a2aa09ab97b3b36ba83b19f6d584239b87f
Gelbrekt <john.ev.forsgren@gmail.com>
2023-03-22 14:07:17 +0100

    test commit

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddDbContext<UserDataContext>(options =>
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle  X 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
